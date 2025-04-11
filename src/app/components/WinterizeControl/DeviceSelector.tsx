97e8f35c1ce1431362c10cfb731d389232ddd88b
davidcasey <david@studiocasey.com>
2025-04-11 10:06:36 -0600

    feat: update device selector to a simple component

--- src/app/components/WinterizeControl/DeviceSelector.tsx
+++ src/app/components/WinterizeControl/DeviceSelector.tsx
@@ -1,4 +1,4 @@
-import { JSX, useEffect, useState } from 'react';
+import { JSX } from 'react';
 import { Device } from 'app/models/rachioModels';
 
 export type DeviceSelectorProps = {
@@ -7,47 +7,22 @@ export type DeviceSelectorProps = {
 }
 
 export const DeviceSelector = ({ devices, onChange }: DeviceSelectorProps): JSX.Element => {
-  // const {setWinterizeSequence} = useContext(WinterizeContext);
-  // const {winterizeSettings} = useContext(WinterizeSettingsContext);
-
-  // const [selectedDevice, setSelectedDevice] = useState<Device | null>(devices.length > 1 ? null : devices[0]);
-  // const [selectedDevice, setSelectedDevice] = useState<Device | null>(null);
-  // const [cycles, setCycles] = useState<number>(0);
-/*
-  useEffect(() => {
-    // console.log('Selected Device:', selectedDevice);
-    // console.log('Cycles:', cycles);
-    if (!selectedDevice) return;
-    const winterizeSteps = Array(cycles).fill(null).map((_, cycleIndex) => (
-      selectedDevice.zones.map((zone: Zone, zoneIndex) => {
-        return {
-          id: `${cycleIndex}-${zoneIndex}`,
-          name: zone.name,
-          active: false,
-          selected: true,
-          blowOutTime: winterizeSettings.blowOutTime,
-          recoveryTime: winterizeSettings.recoveryTime,
-          zone,
-        }
-      })
-    )).flat();
-    const winterizeSequence = {
-      id: selectedDevice.id,
-      name: selectedDevice.name,
-      steps: winterizeSteps,
-    }
-  }, [selectedDevice, cycles]);
-  */
 
   function handleSelectedDevice(e: React.ChangeEvent<HTMLSelectElement>) {
-    console.log('Selected Device:', e.target, devices[e.target.selectedIndex - 1]);
-    onChange(devices[e.target.selectedIndex - 1]);
+    const selectedId = e.target.value;
+    const selectedDevice = devices.find(device => device.id === selectedId);
+    if (selectedDevice) {
+      onChange(selectedDevice);
+    }
   }
 
   return (
-    <select onChange={(e) => {handleSelectedDevice(e)}}>
-      {devices.map((device, index) => (
-        <option key={index} value={device.id}>
+    <select onChange={handleSelectedDevice} defaultValue="">
+        <option value="" disabled>
+          Select device
+        </option>
+      {devices.map((device) => (
+        <option key={device.id} value={device.id}>
           {device.name}
         </option>
       ))}
