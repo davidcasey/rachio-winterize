f9152adfb34877d3213525e4cdb480fe5c71a5e6
davidcasey <david@studiocasey.com>
2025-04-24 14:41:56 -0600

    fix: GAH

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,10 @@ const repoName = 'rachio-winterize';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: isProd ? `/${repoName}` : '',
-  assetPrefix: isProd ? `/${repoName}/` : '',
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  }
+  output: 'export', // Ensure we're using static export
+  basePath: isProd ? `/${repoName}` : '', // Base path for production
+  assetPrefix: isProd ? `/${repoName}/` : '', // Prefix for assets in production
+  trailingSlash: true, // Ensure trailing slashes are used
 };
 
 module.exports = nextConfig;
